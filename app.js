75a410cc2097a08cd5ff1e40a847dd8436a385f2
Suraj <suraj@kilroyblockchain.com>
2023-02-14 06:25:13 +0545

    basic setup of prisma and adminjs

---
+++ app.js
@@ -0,0 +1,22 @@
+const AdminJS = require("adminjs");
+const AdminJSExpress = require("@adminjs/express");
+const express = require("express");
+
+const PORT = 4000;
+
+const start = async () => {
+  const app = express();
+
+  const admin = new AdminJS({});
+
+  const adminRouter = AdminJSExpress.buildRouter(admin);
+  app.use(admin.options.rootPath, adminRouter);
+
+  app.listen(PORT, () => {
+    console.log(
+      `AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`
+    );
+  });
+};
+
+start();
